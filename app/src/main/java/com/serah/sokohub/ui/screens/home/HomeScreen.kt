995bc1b6fc887bb462aed69bafead5f270fce858
Serah-Crypto <ngureserah622@gmail.com>
2026-04-02 19:45:48 +0300

    new

--- app/src/main/java/com/serah/sokohub/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/serah/sokohub/ui/screens/home/HomeScreen.kt
@@ -1,20 +1,47 @@
 package com.serah.sokohub.ui.screens.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.serah.sokohub.R
 import com.serah.sokohub.ui.theme.Darkbrown
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -24,7 +51,7 @@ fun HomeScreen(){
         modifier = Modifier.fillMaxSize()
     ) {
 
-
+       //TopAppBar
         TopAppBar(
             title = { Text(text = "Home") },
             navigationIcon = {
@@ -49,7 +76,7 @@ fun HomeScreen(){
                 }
             },
 
-                    colors = TopAppBarDefaults.topAppBarColors(
+                    colors = topAppBarColors(
                     containerColor = Darkbrown,
 
             titleContentColor = androidx.compose.ui.graphics.Color.White,
@@ -58,26 +85,173 @@ fun HomeScreen(){
         )
 
 
+        )
+        //End of TopAppBar
 
 
+        Spacer(modifier = Modifier.height(20.dp))
 
+        //SearchBar
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        var search by remember { mutableStateOf("") }
+        TextField(
+            value = search,
+            onValueChange = {search = it},
+            modifier = Modifier.fillMaxWidth().padding(start = 20.dp, end = 20.dp),
+            leadingIcon = { Icon(imageVector = Icons.Default.Search, contentDescription = "Search") },//leadingicon-beginning  //trailingicon-end
+            placeholder = {Text(text = "Search Products,Category,...")}
 
         )
 
+        //End of Searchbar
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(
+            text = "Featured Products",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 20.dp)
+
+            )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        //Row
+        Row(
+            modifier = Modifier.horizontalScroll(rememberScrollState())
+        ) {
+            Spacer(modifier = Modifier.width(15.dp))
+
+            Column() {
+                Image(
+                    painter = painterResource(R.drawable.grocery),
+                    contentDescription = "grocery",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Groceries",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(Darkbrown),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+                {
+                    Text(text = "Shop Now")
+                }
+            }
+
+                Spacer(modifier = Modifier.width(15.dp))
+
+
+
+
+            Column() {
+                Image(
+                    painter= painterResource(R.drawable.clothes),
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Clothes",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(Darkbrown),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+                {
+                    Text(text = "Shop Now")
+                }
+
+                
+            }
+
+            Spacer(modifier = Modifier.width(15.dp))
+
+
+            Column() {
+                Image(
+                    painter= painterResource(R.drawable.electronics),
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Electronics",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(Darkbrown),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+                {
+                    Text(text = "Shop Now")
+                }
+
+
+            }
+
+            Spacer(modifier = Modifier.width(15.dp))
+
+            Column() {
+                Image(
+                    painter= painterResource(R.drawable.accessories),
+                    contentDescription = "",
+                    modifier = Modifier.size(200.dp).clip(shape = RoundedCornerShape(20.dp)),
+                    contentScale = ContentScale.Crop
+                )
+                Text(
+                    text = "Accessories",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+
+                Button(
+                    onClick = {},
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(Darkbrown),
+                    modifier = Modifier.padding(start = 20.dp)
+
+                )
+                {
+                    Text(text = "Shop Now")
+                }
+
+
+
+
+            }
+
+
+
+        }
 
 
 
@@ -88,6 +262,8 @@ fun HomeScreen(){
 
 
 
+
+        //End of row
 
 
 
